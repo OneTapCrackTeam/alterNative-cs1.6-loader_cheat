7aa4bc283aeb210df28fb85fd31dc5450d546313
OneTapCrackTeam <63636444+OneTapCrackTeam@users.noreply.github.com>
2021-01-21 16:42:45 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+# onetap Loader remake for OTC
+
+Injection fixed! :)
+
+i was bored and decided to make a remake of the Onetap Loader for OTC users ;)
+
+https://xtremecheats.gg/t/Onetap-Loader-C-Remake-for-OTC-UPDATED
+
+https://i.imgur.com/n4CCOuH.png
+
+https://i.imgur.com/99E0Yze.png
+
+Tutorial:
+
+
+1.go to resources.resx -> drag n` drop your onetap.dll inside of it (name it onetap.dll)
+
+
+2.Built it ;)
+
+Discord: root#1418
---
+++ onetap/Main.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace onetap
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.picturebutton = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picturebutton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(100, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(79, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "𝓪𝓵𝓽𝓮𝓻𝓷𝓪𝓽𝓲𝓿𝓮";
+            // 
+            // picturebutton
+            // 
+            this.picturebutton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picturebutton.Image = global::onetap.Properties.Resources.load;
+            this.picturebutton.Location = new System.Drawing.Point(70, 159);
+            this.picturebutton.Name = "picturebutton";
+            this.picturebutton.Size = new System.Drawing.Size(152, 22);
+            this.picturebutton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picturebutton.TabIndex = 1;
+            this.picturebutton.TabStop = false;
+            this.picturebutton.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(294, 236);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(19)))), ((int)(((byte)(19)))));
+            this.ClientSize = new System.Drawing.Size(294, 236);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.picturebutton);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "alterNative";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picturebutton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox picturebutton;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
